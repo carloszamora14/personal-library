e15a4c6387ae702ca8434ad0de7df9a60c19563f
Carlos Zamora <carlos14zamora@gmail.com>
2024-05-11 23:58:31 -0600

    set express app to parse the request's body

--- src/routes/user.js
+++ src/routes/user.js
@@ -6,4 +6,13 @@ router.get("/login", (req, res) => {
   res.render("pages/index.ejs");
 });
 
+router.get("/signup", (req, res) => {
+  res.render("pages/signup.ejs");
+});
+
+router.post("/users", (req, res) => {
+  console.log(req.body);
+  res.end();
+});
+
 module.exports = router;
