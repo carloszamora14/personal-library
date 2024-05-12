ded07b7eb03a67b52a7ba2db1910b6c8913b025a
CecilLM55 <clopez16@ucol.mx>
2024-05-12 07:44:24 -0600

    home page design

--- src/routes/book.js
+++ src/routes/book.js
@@ -5,4 +5,12 @@ router.get('/home', (req,res)=>{
   res.render('pages/home.ejs')
 })
 
+router.get('/allbooks', (req,res)=>{
+  res.render('pages/allbooks.ejs')
+})
+
+router.get('/gestionatebooks', (req,res)=>{
+  res.render('pages/gestionatebooks.ejs')
+})
+
 module.exports = router;
