7b3c7434923110bd5e53068489bd6cdfd2021999
MAngelesGB <mgutierrez6@ucol.mx>
2024-05-13 18:42:56 -0600

    Add operation Insert to module gestionate books

--- src/routes/book.js
+++ src/routes/book.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const Book = require('../models/books')
 
 router.get('/home', (req,res)=>{
   res.render('pages/home.ejs')
@@ -12,5 +13,23 @@ router.get('/allbooks', (req,res)=>{
 router.get('/gestionatebooks', (req,res)=>{
   res.render('pages/gestionatebooks.ejs')
 })
+
+router.post('/gestionatebooks', async (req, res) => {
+  try {
+    const newBook = new Book({
+      title:req.body.title, 
+      author:req.body.author,
+      genre:req.body.genre, 
+      status:req.body.status
+    });
+
+    const bookSaved =  await newBook.save();  
+    console.log(bookSaved); 
+    /*const Books = await Book.find({});*/
+    res.render('pages/gestionatebooks.ejs');
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
 module.exports = router;
